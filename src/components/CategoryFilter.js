15a94d5824d5239851d82f8624943235f8ce50c7
John <johnkaranjairungu@gmail.com>
2023-04-21 17:49:52 +0300

    added a button

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -4,6 +4,7 @@ function CategoryFilter() {
   return (
     <div className="categories">
       <h5>Category filters</h5>
+      <button type=""></button>
       {/* render <button> elements for each category here */}
     </div>
   );
